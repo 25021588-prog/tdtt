3f6af1c94ed450fb6ba2ee3663a7a63e9bcdde10
25021588-prog <25021588@vnu.edu.vn>
2025-11-09 21:12:32 +0700

    lan two

--- th1.py
+++ th1.py
@@ -1,1 +1,39 @@
-print("")
+#w1a1
+from selectors import SelectSelector
+
+a=7
+b=5
+c=a+b
+print(c)
+#w1a2
+n=4
+t=0
+for i in range(1,n+1):
+    t=t+i
+print(t)
+#w1a3
+city= "Ha Noi"
+year=2025
+print("Thanh pho:",city,"-nam:",year)
+#w1a4
+numbers =[1,2,3,4,5,6]
+for x in numbers:
+    if x%2==0:
+        print(x,"la so chan")
+    else:
+        print(x,"la so le")
+
+#w1a5
+animals =["cat","dog","elephant","tiger"]
+count =0
+for a in animals:
+    count +=1
+print("So luong dong vat la:",count)
+#w1a6
+num =int(input("nhap so:"))
+if num %2==0:
+    print(num,"la so chan")
+else:
+    print(num,"la so le")
+#w1a7
+
